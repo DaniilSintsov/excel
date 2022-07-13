fb273e19d6db6354ad5de98165f6ac853d370f9a
Daniil <daniilpanfilov519@gmail.com>
2022-07-13 16:42:41 +0300

    finish base table logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,14 +1,18 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from './table.template'
 import {resizeHandler} from './table.resize'
-import {shouldResize} from './table.functions'
+import {shouldResize, isCell, matrix, findNextSelector} from './table.functions'
+import {TableSelection} from './TableSelection'
+import {$} from '@core/Dom'
 
 export class Table extends ExcelComponent {
   static className = 'table'
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
-      listeners: ['mousedown']
+      name: 'Table',
+      listeners: ['mousedown', 'keydown', 'input'],
+      ...options
     })
   }
 
@@ -16,9 +20,66 @@ export class Table extends ExcelComponent {
     return createTable(20)
   }
 
+  prepare() {
+    this.selection = new TableSelection()
+  }
+
+  selectCell($cell) {
+    this.selection.select($cell)
+    this.$dispatch('table:select', $cell)
+  }
+
+  init() {
+    super.init()
+    this.selectCell(this.$root.find('[data-id="0:0"]'))
+
+    this.$on('formula:input', text => {
+      this.selection.current.text(text)
+    })
+
+    this.$on('formula:onEnter', () => {
+      this.selection.current.focus()
+    })
+  }
+
   onMousedown(event) {
     if (shouldResize(event)) {
       resizeHandler(this.$root, event)
+    } else if (isCell(event)) {
+      const $target = $(event.target)
+      if (event.shiftKey) {
+        const $cells = matrix($target, this.selection.current)
+          .map(id => this.$root.find(`[data-id="${id}"]`))
+        this.selection.selectGroup($cells)
+      } else {
+        this.selection.select($target)
+      }
     }
+  }
+
+  onKeydown(event) {
+    const keys = [
+      'Enter',
+      'Tab',
+      'ArrowLeft',
+      'ArrowRight',
+      'ArrowDown',
+      'ArrowUp'
+    ]
+
+    const {code} = event
+
+    if (keys.includes(code)) {
+      event.preventDefault()
+
+      const id = this.selection.current.id(true)
+      const $next = this.$root.find(findNextSelector(code, id))
+
+      this.selectCell($next)
+    }
+  }
+
+  onInput(event) {
+    this.$dispatch('table:input', $(event.target))
   }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,10 +4,26 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
+    this.emitter = options.emitter
+    this.unSubscribers = []
+    this.prepare()
+  }
+
+  prepare() {
+
   }
 
   toHTML() {
     return ``
+  }
+
+  $dispatch(event, ...args) {
+    this.emitter.dispatch(event, ...args)
+  }
+
+  $on(event, cb) {
+    const unSub = this.emitter.subscribe(event, cb)
+    this.unSubscribers.push(unSub)
   }
 
   init() {
@@ -16,5 +32,6 @@ export class ExcelComponent extends DomListener {
 
   destroy() {
     this.removeDomListeners()
+    this.unSubscribers.forEach(unSub => unSub())
   }
 }
